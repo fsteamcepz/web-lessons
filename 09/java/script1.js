382918251b3620835a19d303bbd0286fb96c44fe
fsteamcepz <kulish.artem@kmrf.kiev.ua>
2022-04-02 12:46:03 +0300

    Виведення тексту в документ JavaScript

---
+++ 09/java/script1.js
@@ -0,0 +1,12 @@
+document.write('Задача 1');
+
+document.write('<br><i>Задача 2 </i>');
+
+let str = 'text';
+document.write('<br><i>' + str + '</i>');
+
+document.write('<br>1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5');
