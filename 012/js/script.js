06f9ee28bbe39d18be15d626f19c35de71cfd690
fsteamcepz <kulish.artem@kmrf.kiev.ua>
2022-04-19 01:11:17 +0300

    Лаба 7

--- 012/js/script.js
+++ 012/js/script.js
@@ -1,27 +1,80 @@
-/*
-function createPerson (personNmae, personSurname){
-    return{
-        name: personNmae,
-        surname: personSurname,
-        info: function(){ return `${this.name} ${this.surname}`}
+function computerDetails()
+{
+    function createDetail (idDetail, detailName, costOfDetail){
+        return{
+            id: idDetail,
+            name: detailName,
+            price: costOfDetail
+        }
+    }
+
+    let mas = [];
+    let answer;
+
+    answer = +prompt('Введіть кількість деталей');
+
+    if (answer === 0 || answer == null)
+    {
+        alert(`До побачення!`);
+    }
+    else
+    {
+        if(isNaN(answer))
+        {
+            alert(`Помилка. Ви ввели символ!`);
+        }
+        if (+answer < 0)
+        {
+            alert(`Помилка. Ви ввели від'ємне число!`);
+        }
+        else
+        {
+            for (let i = 0; i < answer; i++)
+            {
+                let id = i + 1;
+                let name = prompt('Введіть назву');
+                let cost = +prompt('Введіть вартість');
+                if(isNaN(cost))
+                {
+                    alert('Ви ввели символ!');
+                    cost = +prompt('Введіть вартість');
+                }
+                mas[i] = createDetail(id, name, cost);
+            }
+            document.querySelector('.wrap').innerHTML = `<table class="detail"></table>`;
+            let row = document.createElement('tr');
+            row.innerHTML = `<th>№</th><th>Назва деталі</th><th>Вартість, грн.</th>`;
+            document.querySelector('.detail').appendChild(row);
+
+            for(let j = 0; j < mas.length; j++)
+            {
+                let row2 = document.createElement('tr');
+                row2.innerHTML = `
+                <td>${mas[j].id}</td>
+                <td>${mas[j].name}</td>
+                <td>${mas[j].price}</td>
+                `;
+                document.querySelector('.detail').appendChild(row2);
+            }
+            minD();   // найдешевша деталь
+        }
+    }
+
+    function minD()
+    {
+        let min = mas[0].price;
+        for(let i = 1; i < mas.length; i++)
+        {
+            if(mas[i].price < min)
+            {
+                min = mas[i].price;
+            }
+        }
+        let row3 = document.createElement('tr');
+        row3.innerHTML = `
+        <td colspan="2" style="text-align:right">Найдешева деталь:</td>
+        <td>${min}</td>
+        `;
+        document.querySelector('.detail').appendChild(row3);
     }
 }
-
-let i = 0;
-let masList = [];
-while (i < 3){
-    let surname = prompt('Введіть прізвище');
-    let name = prompt('Введіть імя');
-    let person = createPerson(surname, name);
-    masList.push(person);
-    i++;
-}
-
-for (el of masList)
-{
-    document.write(el.info() + '<br>');
-}*/
-
-// спитати скільки студентів я хочу ввести. Ввести ПІБ студентів
-// через пропуск в одному вікні (Аф Аа Дп)
-// в кінці вивести таблицю ціх студентів
